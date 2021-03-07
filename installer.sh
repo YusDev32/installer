86a8d490e07cd0dd983577690d5756b5e76f68f6
YusDev32 <80189728+YusDev32@users.noreply.github.com>
2021-03-07 13:31:57 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+clear
+echo -e "\033[1;93m ×============================>
+echo -e "\033[1;90m[\033[1;96m•\033[1;90m] \033[1>
+echo -e "\033[1;90m[\033[1;96m•\033[1;90m] \033[1>
+echo -e "\033[1;90m[\033[1;96m•\033[1;90m] \033[1>
+echo -e "\033[1;93m ×============================>
+echo -e " \033[1;96m [1] Bahan "
+echo -e " \033[1;96m [2] SqlMap "
+echo -e " \033[1;96m [3] DDOS "
+echo -e " \033[1;96m [4] keybor plus "
+echo -e " \033[1;96m [5] wpbf"
+echo -e " \033[1;96m [6] exit"
+read -p  " ~>" pilih
+if [ $pilih  == "1" ]
+then
+
+cd $HOME
+   pkg update && upgrade
+   pkg install python python2 ruby figlet toilet >
+   pip install mechanize
+   pip install requests
+   pip2 install mechanize
+   pip2 install requests
+
+    elif [ $pilih == "2" ]
+    then
+    cd $HOME
+    git clone https://github.com/sqlmapproject/sq>
+
+    elif [ $pilih == "3" ]
+    then
+    cd $HOME
+
+elif [ $pilih == "4"
+  then
+        cd $HOME
+   git clone https://github.com/karjok/terkey
+
+elif [ $pilih == "5" ]
+   then
+    cd $HOME
+
+elif [ $pil =="6" ]
+then
+    echo -e "terima kasih sudah menggunakan tools>
+    sleep 2
+    echo -e "jangan lupa mampir ke WhatsApp Admin>
+    xdg-open https://wa.me/+6285695258843
+    sleep 5
+else
+    echo "salah, masukan nomor input dengan benar"
+
+fi
+clear
+   
+   
+   
+   
